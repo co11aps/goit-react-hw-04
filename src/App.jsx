89bcf6fd92ec7012e96266f5541f9ff9246c7265
co11aps <143988142+co11aps@users.noreply.github.com>
2024-04-04 09:55:06 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import "./App.css";
+import axios from "axios";
+import { useState, useRef, useEffect } from "react";
 
-function App() {}
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        "<https://hn.algolia.com/api/v1/search?query=react>"
+      );
+      setArticles(response.data.hits);
+      console.log(response.statusText);
+    }
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+
+      <ul></ul>
+    </div>
+  );
+};
 
 export default App;
